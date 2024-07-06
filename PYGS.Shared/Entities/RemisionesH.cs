8a56000111c8636f3cb76ae568655d8fce7fca60
Christian David Morales <christtian.morales@gmail.com>
2024-07-06 10:58:39 -0400

    Cambios API y Shared V1

--- PYGS.Shared/Entities/RemisionesH.cs
+++ PYGS.Shared/Entities/RemisionesH.cs
@@ -14,25 +14,29 @@ namespace PYGS.Shared.Entities
 
         public DateTime fecha { get; set; }
 
+        public DateTime fechaConta { get; set; }
+
         public int NroFolio { get; set; }
 
-        public string establecimiento { get; set; }
+        public string? establecimiento { get; set; }
 
-        public string emision { get; set; }
+        public string? emision { get; set; }
 
-        public string timbrado { get; set; }
+        public string? timbrado { get; set; }
 
-        public string CardCode { get; set; }
+        public string? CardCode { get; set; }
 
-        public string CardName { get; set; }
+        public string? CardName { get; set; }
 
-        public string RUC { get; set; }
+        public string? RUC { get; set; }
 
-        public string Chofer { get; set; }
+        public string? Chofer { get; set; }
 
-        public string Marca { get; set; } 
+        public string? Marca { get; set; } 
 
-        public string Matricula { get; set; }
+        public string? Matricula { get; set; }
+
+        public int FolioNum { get; set; }
 
         public ICollection<RemisionesD>? remisionesD { get; set; }
 
